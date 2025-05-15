dfa921a1ed09a44eba58190b7d10e8ff8246b115
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2025-05-15 18:35:07 +0200

    add fix

--- src/utils/Routes/PrivatRoute.js
+++ src/utils/Routes/PrivatRoute.js
@@ -3,8 +3,8 @@ import { Navigate, Outlet } from "react-router-dom";
 import LoaderComponent from "../../components/Loader";
 
 const PrivatRoute = () => {
-  const { isLogin } = useSelector((state) => state.auth);
-  const { accessToken } = useSelector((state) => state.auth);
+  const { isLogin } = useSelector((state) => state.auth || {});
+  const { accessToken } = useSelector((state) => state.auth || {});
 
   // const { isKidsLogin } = useSelector((state) => state.kids);
   // const { kidsAccessToken } = useSelector((state) => state.kids);
